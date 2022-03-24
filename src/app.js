cb1597f48a5749e40086555067a492552acc8e06
Amanpreet <abinepal@my.bcit.ca>
2022-03-23 18:50:39 -0700

    did part three of lab

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
-//part 2 of lab
+//part 2
 
 const singleElem = document.querySelector("#side-nav")
 console.log(singleElem)
 
+//part 3
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+
+
+
+function stuAdd() {
+    const studentsScholarship = []
+    for (const stu of students) {
+        let avg = 0
+        for (const grade of stu.grades) {
+            avg += grade
+        }
+        avg = avg/stu.grades.length
+        if (avg >= 80) {
+            studentsScholarship.push({name: stu.name, gradeAvg: avg})
+        }
+    }
+    
+    return studentsScholarship
+}
+
+console.log(stuAdd())
 
